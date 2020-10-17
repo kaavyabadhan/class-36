ce4f526a17dd4b65cbcc89d93cbec67668422294
kaavyabadhan <71015505+kaavyabadhan@users.noreply.github.com>
2020-10-17 15:01:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var gameState = 0
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+  game = new Game()
+  game.getState()
+  game.start()
+}
+
+function draw(){
+ 
+}
